94987d1116b93470dbc73875d4bb6bf260bf307e
Carlos André <161266479+carlosandret@users.noreply.github.com>
2025-02-04 01:07:31 -0300

    MUDANÇAS

--- scripts.js
+++ scripts.js
@@ -2,49 +2,107 @@ function getByID(id) {
 return document.getElementById(id);
 }
 
-let botaoConsultar = getByID('botaoConsultar');
-botaoConsultar.addEventListener('click', consultarPreco);
-async function consultarPreco() {
-    let moedaBase = getByID('moedaBase').value.toUpperCase(); // Moeda base (ex.: BTC)
-    let moedaConversao = getByID('moedaConversao').value.toUpperCase(); // Moeda de conversão (ex.: USDT)
-    let resultado = getByID('resultado');
-    let url =
-    `https://api.binance.com/api/v3/ticker/price?symbol=${moedaBase}${moedaConversao}`;
-try {
-let response = await fetch(url); // Faz a requisição à API Binance
-if (!response.ok) {
-throw new Error(`HTTP Error: ${response.status} - ${response.statusText}`);
+let url = 'https://jsonplaceholder.typicode.com/'
+
+async function buscaPosts() {
+    try {
+        let response = await fetch(url + 'posts'); // Faz a requisição dos posts da API
+        if (!response.ok) {
+        throw new Error(`HTTP Error: ${response.status}`);
+    }
+    // Transforma os dados em json
+    let posts = await response.json();
+
+    return posts
+    } catch (error) {
+        console.error('Erro ao buscar posts:', error);
+    }
 }
-let json = await response.json();
 
-resultado.innerHTML = `
-<p><strong>Par de Moedas:</strong> ${json.symbol}</p>
-<p><strong>Preço:</strong> ${parseFloat(json.price).toFixed(2)}
-${moedaConversao}</p>`;
-} catch (error) {
-resultado.innerHTML = 'Erro: ' + error.message;
+async function buscarAutores(autorId) {
+    try {
+        let response = await fetch(url + `users/${autorId}`); // Faz a requisição dos autores 
+        if (!response.ok) {
+        throw new Error(`HTTP Error: ${response.status}`);
+    }
+    // Transforma os dados em json
+    let autores = await response.json();
+
+    return autores
+    } catch (error) {
+        console.error('Erro ao buscar autores:', error);
+    }
 }
-};
 
-// a) Botão para limpar os campos
-let botaoLimparCampos = getByID('botaoLimparCampos');
-botaoLimparCampos.addEventListener('click', ()=> {
-    moedaBase.value = '';
-    moedaConversao.value = '';
-    resultado.innerHTML = '';
-});
+async function buscarComentarios(postId) {
+    try {
+        let response = await fetch(url + `comments?postId=${postId}`); // Faz a requisição dos autores 
+        if (!response.ok) {
+        throw new Error(`HTTP Error: ${response.status}`);
+    }
+    // Transforma os dados em json
+    let comentarios = await response.json();
 
-// b) Botão de inverter moedas: Um botão que troca os valores das moedas base e de conversão;
-let boataoInverterValor = getByID('botaoInverteValor');
-boataoInverterValor.addEventListener('click', ()=> {
-    let moedaBase = getByID('moedaBase');
-    let moedaConversao = getByID('moedaConversao');
+    return comentarios
+    } catch (error) {
+        console.error('Erro ao buscar comentarios:', error);
+    }
+}
 
-    let moedaTemp = moedaBase.value;
-    moedaBase.value = moedaConversao.value;
-    moedaConversao.value = moedaTemp;   
-});
+async function exibePosts() {
+    let posts = await buscaPosts();
 
+    let quantidade = 6;
 
+    for (let i = 1; i <= quantidade; i++) {
+        let titulo = getByID(`tituloPost${i}`);
+        let body = getByID(`bodyPost${i}`);
+        let conteudo = getByID(`conteudo${i}`);
 
+        // Pegar o post correspondente
+        let post = posts[i - 1]
 
+        titulo.innerHTML = post.title;
+        body.innerHTML = post.body;
+
+        let autor = await buscarAutores(post.userId)
+
+        conteudo.innerHTML = `
+            <p><strong>AUTOR:</strong> ${autor.username}</p>
+            <p><strong>EMAIL:</strong> ${autor.email}</p>
+            `;
+
+        // Inclusão de comentários
+        try {   
+            let listaComentarios = await buscarComentarios(post.id);
+            let linkComentario = getByID(`linkComentario${i}`);
+
+            linkComentario.addEventListener('click', async (evento) =>{
+                evento.preventDefault();
+                let divcomentarios = getByID(`comentarios${i}`);
+
+                if (divcomentarios.style.display === 'none'){
+                    divcomentarios.innerHTML = `<h3>Comentários: </h3>`;
+                    
+                    listaComentarios.forEach(comentario => {
+                        divcomentarios.innerHTML += `
+                            <p>(${comentario.email}) - <strong>${comentario.name}</strong></p>
+                            <p>${comentario.body}</p>`;
+                    });
+
+                    divcomentarios.style.display = 'block';
+                    linkComentario.textContent = 'Ocultar comentários'
+                } else {
+                    // Oculta os comentários
+                    divcomentarios.style.display = 'none';
+                    linkComentario.textContent = 'Acessar comentários'
+                }
+                    
+            })
+        } catch (error) {
+            listaComentarios.innerHTML = `<p>Erro: ${error.message}</p>`;
+        }      
+    };
+}
+
+exibePosts()
